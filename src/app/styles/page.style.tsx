575955954d848c06c2333229f5e18d82250f3a4b
GustavoSMelo <gsantos15569@gmail.com>
2023-08-23 00:52:22 -0300

    remove green text-decoration

--- src/app/styles/page.style.tsx
+++ src/app/styles/page.style.tsx
@@ -65,7 +65,6 @@ export const Dropbox = styled.div`
 
     .fileInformation > span {
         margin-left: 10px;
-        text-decoration: dotted #2AE310;
     }
 
     @media screen and (max-width: 768px) {
